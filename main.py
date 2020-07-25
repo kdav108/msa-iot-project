87dba87272c0a4b46bb0c2cd9502b9666effa288
Kunj Dave <kdav0006@student.monash.edu>
2020-07-25 12:18:41 +1000

    Removed extra sensor as it was not very practical

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 # Date created: 22/07/20
 
 # Import all dependencies
-from azure.iot.device import IoTHubDeviceClient, Message
 import time
 import random
+# Using the Python Device SDK for IoT hub: https://github.com/Azure/azure-iot-sdk-python
+# This helps connect the data generated here to a device-specific MQTT endpoint on the IoT Hub
+from azure.iot.device import IoTHubDeviceClient, Message
+
 
 # Define device connection strings
-lightSensor = "HostName=msa-project-iot-hub.azure-devices.net;DeviceId=LightSensor;SharedAccessKey=TMH6FAeq0kbF/+E6cbP5tVgTWnAfLi2ZI3lF5OxMpgI="
-soilSensor = "HostName=msa-project-iot-hub.azure-devices.net;DeviceId=SoilDevice;SharedAccessKey=9Of1GyYiq4Z70nXL8u82KtgqD/u+t+GLPp81+m3MqO0="
-message_light = '{{"LightIntensity": {intensity}}}'
-message_soil = '{{"SoilTension": {tension}}}'
-# message_light = '{{"LightIntensity": {intensity}, "SoilTension": {tension}}}'
+environmentSensor = "HostName=msa-project-iot-hub.azure-devices.net;DeviceId=LightSensor;SharedAccessKey=TMH6FAeq0kbF/+E6cbP5tVgTWnAfLi2ZI3lF5OxMpgI="
+message = '{{"LightIntensity": {intensity}, "SoilTension": {tension}}}'
 
 # Create base data
 init_lightIntensity = 50  # In 1000 lux
@@ -25,35 +25,34 @@ init_overcast = False  # Represents whether the weather is becoming more overcas
 
 def run_simulation(overcast, lightIntensity, soilTension):
     try:
-        client_light_sensor = IoTHubDeviceClient.create_from_connection_string(lightSensor)
-        client_tension_sensor = IoTHubDeviceClient.create_from_connection_string(soilSensor)
+        client = IoTHubDeviceClient.create_from_connection_string(environmentSensor)
         print("Light and soil sensor sending periodic messages (press Ctrl-C to exit)" )
 
         while True:
             if not overcast:
                 # Sunlight keeps increasing
-                lightIntensity += random.random()
+                lightIntensity += random.random()/100
 
                 # There's some chance clouds start obscuring the sky again
                 # Probability of change is low since if the weather was overcast before it would tend to stay overcast
                 # the next minute.
-                if random.random() < 0.01:
+                if random.random() < 0.05:
                     overcast = True
 
                 # Soil Moisture generally increases if sun light intensity increases
                 # Soil Moisture is measured in kPa so would not fluctuate a lot (i.e an increase/decrease of somewhere
-                # between 0.01 and 0.1 is a reasonable assumption)
-                soilTension += random.random()/10
+                # between 0.001 and 0.01 is a reasonable assumption)
+                soilTension += random.random()/100
 
             else:
                 #  Sunlight decreases
-                lightIntensity -= random.random()/10
+                lightIntensity -= random.random()/100
 
                 # There's some chance clouds start disappearing
-                if random.random() < 0.01:
+                if random.random() < 0.05:
                     overcast = False
 
-                soilTension -= random.random()
+                soilTension -= random.random()/100
 
             # Check if soilMoisture is still valid (it cannot exceed 80 or fall below 0 is using a Tensiometer)
             soilTension = min(soilTension, 80)
@@ -64,29 +63,27 @@ def run_simulation(overcast, lightIntensity, soilTension):
             lightIntensity = max(lightIntensity, 0.001)  # Has to be greater than 1 lux
 
             # Packet measurements
-            # data = Message(message.format(intensity=lightIntensity, tension=soilTension))
-            intensity_data = Message(message_light.format(intensity=lightIntensity))
-            soil_data = Message(message_soil.format(tension=soilTension))
+            data = Message(message.format(intensity=lightIntensity, tension=soilTension))
+            # intensity_data = Message(message_light.format(intensity=lightIntensity))
+            # soil_data = Message(message_soil.format(tension=soilTension))
 
             # Check if soil tension which is roughly related to it's dryness is within good limits
             if soilTension > desiredTensionMax:
-                soil_data.custom_properties["drynessAlert"] = "True"
-                soil_data.custom_properties["wetnessAlert"] = "False"
+                data.custom_properties["drynessAlert"] = "True"
+                data.custom_properties["wetnessAlert"] = "False"
             elif soilTension < desiredTensionMin:
-                soil_data.custom_properties["drynessAlert"] = "False"
-                soil_data.custom_properties["wetnessAlert"] = "True"
+                data.custom_properties["drynessAlert"] = "False"
+                data.custom_properties["wetnessAlert"] = "True"
             else:
-                soil_data.custom_properties["drynessAlert"] = "False"
-                soil_data.custom_properties["wetnessAlert"] = "False"
+                data.custom_properties["drynessAlert"] = "False"
+                data.custom_properties["wetnessAlert"] = "False"
 
             # Send packet data
-            # client.send_message(data)
-            client_light_sensor.send_message(intensity_data)
-            client_tension_sensor.send_message(soil_data)
-            print(soil_data)
-            print(intensity_data)
+            client.send_message(data)
+            print(data)
 
-            # Wait 1 second before sending next message
+
+            # Wait a second before sending next message
             time.sleep(1)
 
     except KeyboardInterrupt:
